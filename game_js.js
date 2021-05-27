0531371a89df5c7cb1fd900f8d71f743104c83b9
VirajBhakta <viraj_bhakta94@hotmail.co.uk>
2021-05-27 16:55:57 +0100

    uploading a working version of the game, not the cleanest or best code as trial and error

--- game_js.js
+++ game_js.js
@@ -46,36 +46,37 @@ function playRound(playerSelection, computerSelection){
 
 }
 
+
 /*
 -1 You
 0 Draw
 1 Comp
 */
 
-function playRoundNumeric(playerSelection, computerSelection, playerCnt, compCnt){
+function playRoundNumeric(playerSelection, computerSelection){
     playerSelection = propcase(playerSelection);
 
     if (playerSelection==computerSelection){
         return 0
     }
     else if (playerSelection=='Rock' && computerSelection=='Paper') {
-        return ++compCnt;
+        return 1;
     }
     else if (playerSelection=='Paper' && computerSelection=='Scissors') {
-        return ++compCnt;
+        return 1;
 
     }
     else if (playerSelection=='Scissors' && computerSelection=='Rock') {
-        return ++compCnt;
+        return 1;
     }
     else{
-        return ++playerCnt;
+        return -1;
     }
 
 
 }
 
-
+//User prompt function take only correct values do until cancel or correct input
 function userPrompt(){
     let answerCheck=1;
     let answer = prompt("Please enter, Rock, Paper or Scissors");
@@ -102,9 +103,11 @@ function userPrompt(){
     }
 }
 
-//userPrompt();
 
-function game(rounds=5){
+
+//Main game function below
+
+function game(rounds=5, input){
     console.log('Welcome you are about to play 5 rounds of Rock Paper Scissors with the computer');
 
     let playerCnt = 0;
@@ -114,13 +117,22 @@ function game(rounds=5){
     let roundOutput;
     for(let i=0; i!=rounds; ++i){
         console.log(`Round ${i}`);
-        userInput = userPrompt();
+        userInput = input
         computer = computerPlay();
         console.log(playRound(userInput, computer));
 
-        roundOutput=playRoundNumeric(userInput, computer, playerCnt, compCnt);
-
+        roundOutput=playRoundNumeric(userInput, computer);
+        if (roundOutput==0){
+            continue;
+        }
+        else if (roundOutput==1){
+            ++compCnt;
+        }
+        else if (roundOutput==-1){
+            ++playerCnt;
+        }
     }
+
     console.log('Game Over');
     if (playerCnt>=compCnt){
         console.log(`You Won! ${playerCnt} Vs ${compCnt}`);
@@ -134,12 +146,160 @@ function game(rounds=5){
 
 }
 
-game();
-//console.log(game());
+function changeDom(out){
+    const container=document.querySelector('.container');
+
+    const outputDiv = document.createElement('div');
+    outputDiv.classList.add('content');
+    outputDiv.textContent=out;
+    container.appendChild(outputDiv);
+  
+  }
+
+  let clicks=0;
+  function hello() {
+    clicks += 1;
+    //document.getElementById("clicks").textContent= clicks;
+    return clicks;
+      
+  };
+
+
+  let playerCnt=0;
+  function playerC() {
+    playerCnt += 1;
+    return playerCnt;
+      
+  };
+
+  let compCnt=0;
+  function compC() {
+    compCnt += 1;
+    return compCnt;
+      
+  };
+
+
+  function updateBox(input){
+    const container = document.querySelector('.listOutput');
+    let textAdd=document.createElement('li');
+    textAdd.classList.add('li' + input.id);
+    textAdd.textContent = playRound(input.id, computerPlay());
+    container.appendChild(textAdd);
+
+  }
+
+  let outRet;
+  let comptest;
+  let playertest;
+  let result;
+  function test(counter, input){
+      if (counter<=5){
+        let computer = computerPlay();
+        result = playRound(input.id,computer);
+        roundOutput=playRoundNumeric(input.id, computer);
+        if (roundOutput==1){
+            comptest = compC();
+            console.log(comptest);
+        }
+        else if (roundOutput==-1){
+            playertest = playerC();
+            console.log(playertest);
+        }
+      }
+      else if (counter==6){
+        console.log('Game Over');
+        if (playertest>comptest){
+            result = `You Won! ${playertest} Vs ${comptest}`;
+        }
+        else if (playertest<comptest){
+            result = `You Lost! ${playertest} Vs ${comptest}`;
+        }
+        else if (playertest==comptest){
+            result=`You Draw! ${playertest} points each`;
+        }
+     
+      }
+    
+    return result;
+
+  }
+
+
+function alertTest(playerCnt, compCnt){
+    const inputs = document.querySelectorAll('input');
+    const container = document.querySelector('.listOutput');
+
+    inputs.forEach( (input) => {
+        // and for each one we add a 'click' listener
+    input.addEventListener('click', () => {
+        counter = hello();
+        //test(counter, input);
+        //console.log(playRound(input.id,computerPlay()));
+        if (counter<=5){
+            let textAdd=document.createElement('li');
+            textAdd.classList.add('li' + input.id);
+            textAdd.textContent = test(counter, input);
+            container.appendChild(textAdd);
+
+        }
+        else if (counter==6){
+            let textAdd=document.createElement('p');
+            textAdd.classList.add('p' + input.id);
+            textAdd.textContent = test(counter, input);
+            textAdd.setAttribute('style', 'font-size: 30px');
+            container.appendChild(textAdd);
+
+            let butAdd = document.createElement('button');
+            butAdd.textContent = 'Play Again';
+            butAdd.onclick=function(){window.location.reload()};
+            butAdd.setAttribute('style', 'margin-top: 30px'); 
+            container.appendChild(butAdd);
+            
+
+
+
+
+
+        }
+
+    });
+  });
+}
+
+
+alertTest(playerCnt, compCnt);
+
+
+
+
+
 
 /*
-let viraj = 'Scissors';
-const computer = computerPlay();
+const container = document.querySelector('.listOutput');
 
-console.log(playRound(viraj, computer));
+let compCnt=0;
+let playerCnt=0;
+
+const inputs = document.querySelectorAll('input');
+inputs.forEach( (input) => {
+      // and for each one we add a 'click' listener
+  input.addEventListener('click', () => {
+    let textAdd=document.createElement('li');
+    textAdd.classList.add('li' + input.id);
+    textAdd.textContent = playRound(input.id, computerPlay());
+    container.appendChild(textAdd);
+
+    let textNumbAdd = document.createElement('li');
+    textNumbAdd.classList.add('li_num'+input.id);
+    textNumbAdd.textContent=playRoundNumeric(input.id, computerPlay());
+    let test;
+
+    test = playRoundNumeric(input.id, computerPlay());
+    container.appendChild(textNumbAdd)
+    
+  });
+});
 */
+
+
